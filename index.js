5cf16e3784ced64687bc509be78049754ac81ec3
NEHA RAHMAN <neha@Nehas-MacBook-Air.local>
2023-09-26 21:25:32 +0530

    ci/cd pipeline

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('hello ci/cd pipeline')
+})
+
+app.get('/home',(req,res)=>{
+    res.send('hello home')
+})
+
+app.listen(9000,()=>{
+    console.log('listening to the server')
+})
